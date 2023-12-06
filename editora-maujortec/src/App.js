12e2503be701996559cdb6f009c3657ef6ffde19
mamgjunior <mamgjunior@hotmail.com>
2023-12-05 22:11:55 -0300

    Criando a estrutura do site

--- editora-maujortec/src/App.js
+++ editora-maujortec/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import Principal from './components/Principal';
+import Rodape from './components/Rodape';
+import Topo from './components/Topo';
 
 
 function App() {
   return (
     <>
-      <header className='topo'>
-        <h1 className='logo'></h1>
-        <ul>
-          <li><a href=''>Navegação</a></li>
-        </ul>
-      </header>
-
-      <main className='principal'>
-        <h2>Últimos lançamentos</h2>
-        <div className='card'>Card</div>
-      </main>
-
-      <footer className='rodape'>
-        <p>Contéudos cedidos pela Editora Novatec &#8212; Copyright 2020</p>
-      </footer>
+      <Topo />
+      <Principal />
+      <Rodape />
     </>
   );
 }
