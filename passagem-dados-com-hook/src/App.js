4648ec00088a945e1b88f780350d87434126e506
mamgjunior <mamgjunior@hotmail.com>
2023-12-26 23:31:52 -0300

    Criando o projeto passagem de dados com hooks

---
+++ passagem-dados-com-hook/src/App.js
@@ -0,0 +1,15 @@
+import EntradaDado from './components/EntradaDado';
+import SaidaDado from './components/SaidaDado';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <EntradaDado />
+      <SaidaDado />
+    </>
+  );
+}
+
+export default App;
